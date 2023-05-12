9b69a600053006eec54b118bf30cf4246cd6fdf9
eddyboa42 <fellipe.fausto10@gmail.com>
2023-05-12 15:20:15 -0300

    addded constructors

--- ATIV_1/main.cpp
+++ ATIV_1/main.cpp
@@ -5,12 +5,41 @@ using namespace std;
 class Aluno{
     string nomeCompleto, cfp, idade, serie;
     long int matricula;
+
+    public:
+
+    Aluno(){
+
+    }
+
+    Aluno(string nomeCompleto, string cfp, string idade, string serie, long int matricula){
+        this -> nomeCompleto = nomeCompleto;
+        this -> cfp = cfp;
+        this -> idade = idade;
+        this -> serie = serie;
+        this -> matricula = matricula;
+    }
 };
 
 class Funcionario{
     string nomeCompleto, cfp, idade, cargo;
     long int matricula;
     float salario; 
+    
+    public:
+
+    Funcionario(){
+
+    }
+
+    Funcionario(string nomeCompleto, string cfp, string idade, string cargo, long int matricula, float salario){
+        this -> nomeCompleto = nomeCompleto;
+        this -> cfp = cfp;
+        this -> idade = idade;
+        this -> cargo = cargo;
+        this -> matricula = matricula;
+        this -> salario = salario;
+    }
 };
 
 int main(){
