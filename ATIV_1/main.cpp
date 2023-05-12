4702bdb1763a9c4b3906659af0b3f099d60ed6a9
eddyboa42 <fellipe.fausto10@gmail.com>
2023-05-12 15:16:21 -0300

    first commit

---
+++ ATIV_1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Aluno{
+    string nomeCompleto, cfp, idade, serie;
+    long int matricula;
+};
+
+class Funcionario{
+    string nomeCompleto, cfp, idade, cargo;
+    long int matricula;
+    float salario; 
+};
+
+int main(){
+
+
+    return 0;
+}
